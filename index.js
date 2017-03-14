605a6aa655bc1d2b5586c2d4df22ef18d6fb2adf
Alice Maldonado <the.alice.maldonado@gmail.com>
2017-03-14 16:25:43 -0700

    cli building

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
+#!/usr/bin/env node
+
 // # node rename.js
 // node shell scripting
-//
+
 // Ensure that files are able to be modified
 // Possible chmod needs to happen too
 
+const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 let folder = path.join(`${folder}`); // Folder the script reads to replace text
 let regex = /_/gi; // rename.js finds `regex` to replace with `replaceWith`, which replaces with whatver
-let replaceWith = `${string}`;
+let replace = `${string}`;
 
-var dirSync = fs.readdirSync(folder);
-for (i=0; i < directory.length; i++) {
-  if (directory[i].match(regExp)) {
-    fs.renameSync(
-      path.join(folder, directory[i]),
-      path.join(folder, directory[i].replace(regex, replaceWith))
-    );
+let rename;
+
+program
+  .version('0.0.1')
+  .usage('[options] <directory> <destination>')
+  .option('-c --copy', 'Makes a copy')
+  .parse(process.argv);
+
+massRename = (folder, regex, replace) => {
+  let directory = fs.readdirSync(folder);
+  for (i=0; i < directory.length; i++) {
+    if (directory[i].match(regExp)) {
+      fs.renameSync(
+        path.join(folder, directory[i]),
+        path.join(folder, directory[i].replace(regex, replace))
+      );
+    };
   };
+};
+
+exports.module = (folder, regex, replace) => {
+  rename(folder, regex, replace);
 };
