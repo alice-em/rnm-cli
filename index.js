052225e4f2f340a1743345b9ac7ed9a30230a44d
alice-em <the.alice.maldonado@gmail.com>
2017-03-21 16:14:31 -0700

    core principles test

--- index.js
+++ index.js
@@ -9,9 +9,9 @@
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
-let folder = path.join(`${folder}`); // Folder the script reads to replace text
-let regex = /_/gi; // rename.js finds `regex` to replace with `replaceWith`, which replaces with whatver
-let replace = `${string}`;
+let folder = '/media/alicemod/Ajax/Comic Database/Action Comics (2016)'; // Folder the script reads to replace text
+let regex = /\(.+\)|[^a-zA-Z0-9.\s]/gi; // rename.js finds `regex` to replace with `replaceWith`, which replaces with whatver
+let replace = '';
 
 let rename;
 
@@ -24,15 +24,31 @@ program
 massRename = (folder, regex, replace) => {
   let directory = fs.readdirSync(folder);
   for (i=0; i < directory.length; i++) {
-    if (directory[i].match(regExp)) {
+    if (directory[i].match(regex)) {
+      console.log(`Processing ${directory[i]}`);
+
+      let ext = path.extname(directory[i]); // Extension
+      let a = path.basename(directory[i], ext).replace(regex, replace); // basename with regex applied
+      let b = a.charAt(a.length - 1); // Last character
+      if (b == ' ') {
+        a = a.slice(0, -1); // Removes ' '
+      } else {
+        // Nothing really
+      };
+
       fs.renameSync(
         path.join(folder, directory[i]),
-        path.join(folder, directory[i].replace(regex, replace))
+        path.join(folder, a + ext)
       );
-    };
+      console.log(`${directory[i]} => ${a + ext}`);
+    } else {
+      console.log('No valid target files.');
+    }
   };
 };
 
-exports.module = (folder, regex, replace) => {
-  rename(folder, regex, replace);
-};
+// exports.module = (folder, regex, replace) => {
+//   rename(folder, regex, replace);
+// };
+
+massRename(folder, regex, replace);
